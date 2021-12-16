06f6457b4b036fbcfb80988c6bbb200cfa314712
“kostik700015” <kostik700015@gmail.com>
2021-12-16 10:24:50 -0500

    adding todo to each category added, calendar removed from All todo page

--- application/src/App.js
+++ application/src/App.js
@@ -1,10 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import TodoContainer from './components/TodoContainer'
 import Header from './components/Header'
+import Calendar from 'react-calendar';
 import './App.css'
 import {
   BrowserRouter,
-  Route
+  Route,
+  Redirect
 } from "react-router-dom";
 
 function App() {
@@ -14,6 +16,12 @@ function App() {
   const [displayTodo, setDisplayTodo] = useState([]);
   const [choosedCategory, setChoosedCategory] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
+  const [value, onChange] = useState(new Date());
+
+  const handleDate = (event) => {
+    // console.log(event)
+    onChange(event)
+  };
 
   useEffect(()=>{
     const headers = { 'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}` }
@@ -36,15 +44,16 @@ function App() {
         {
           "fields": {
             "Categories": [
-              choosedCategory
-            ],
+              choosedCategory, // include choosed Category
+              categoryList[0].id // want always include All Category
+           ],
             "Title": newTodoTitle,
             "Date": date.toISOString().substring(0, 10)
           },
         },
       ]
     }
-    console.log(newtodo)
+    // console.log(newtodo)
 
     fetch(`https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Todos`,
     { method: 'POST',
@@ -56,7 +65,7 @@ function App() {
     }).then(res => res.json())
       .then(res =>setChanged(false));
   }
-  // console.log(displayTodo)
+  console.log(categoryList)
   function removeTodo(id) {
     const newList = displayTodo.filter(
       (todo) => todo.id !== id
@@ -76,6 +85,15 @@ function App() {
     setDisplayTodo(array)
   }
 
+  const filterByCategory  = (todos) => {
+    var array = [];
+    todos.forEach( (todoId) => {
+      var temp = todoList.filter((todo) => todo.id === todoId)
+      array.push(temp[0])
+    })
+    return array
+  }
+
   return (
     <BrowserRouter>
     <Header categories={categoryList} chooseCategory={chooseCategory}/>
@@ -84,17 +102,28 @@ function App() {
           addTodo={addTodo}
           displayTodo={displayTodo}
           removeTodo={removeTodo}
-          isLoading={isLoading} 
+          isLoading={isLoading}
+          root='/'
         />
       </Route>
       {categoryList.map((category)=>(
         <Route key={category.id} path={`/${category.fields.Name}`}>
+          {category.fields.Name === "All" ? <Redirect to="/" /> :
+          <>
+          <Calendar
+            onChange={handleDate}
+            value={value}
+            className="react-calendar"
+          />
           <TodoContainer
             addTodo={addTodo}
-            displayTodo={displayTodo}
+            // displayTodo={displayTodo}
+            displayTodo={filterByCategory(category.fields.Todos)}
             removeTodo={removeTodo}
             isLoading={isLoading}
+            value={value}
           />
+          </> }
         </Route>
       ))}
     </BrowserRouter>
--- application/src/components/TodoContainer.js
+++ application/src/components/TodoContainer.js
@@ -2,29 +2,16 @@ import React from 'react'
 import { useState } from 'react';
 import AddTodoForm from './AddTodoForm';
 import TodoList from'./TodoList'
-import Calendar from 'react-calendar';
 import PropTypes from 'prop-types';
 
-function TodoContainer({addTodo, displayTodo, removeTodo, isLoading}){
-  console.log(displayTodo)
-  const [value, onChange] = useState(new Date());
-
-  const handleDate = (event) => {
-    console.log(event)
-    onChange(event)
-  };
+function TodoContainer({addTodo, displayTodo, removeTodo, isLoading, value, root}){
+  // console.log(temp)
+  
   return (
-    <>
-    <Calendar
-        onChange={handleDate}
-        value={value}
-        className="react-calendar"
-      />
     <div className="todo">
-      <AddTodoForm onAddTodo={addTodo} value={value}/>
+      {root === '/' ? <div style={{marginTop: '40px'}}></div> : <AddTodoForm onAddTodo={addTodo} value={value}/>}
       {isLoading ? <span>Loading...</span> : <TodoList todoList={displayTodo} onRemoveTodo={removeTodo}/>}
     </div>
-    </>
   )
 }
 
